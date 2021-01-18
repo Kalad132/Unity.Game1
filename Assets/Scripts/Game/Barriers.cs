a717ba381138b13bf114448da9106f5f28ebf8a0
Kalad132 <67950598+Kalad132@users.noreply.github.com>
2021-01-18 15:26:48 +0300

    Second-branch init

---
+++ Assets/Scripts/Game/Barriers.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barriers : MonoBehaviour
+{
+    [SerializeField] private GameObject _barrierTemplate;
+    [SerializeField] private float _spawnDistance;
+    [SerializeField] private float _spawnPeriod;
+    [SerializeField] private float _spawnPeriodRandom;
+    [SerializeField] private Transform _player;
+    [SerializeField] private int _maxBarriers;
+
+    private float _spawnTrigger;
+    private List<Transform> _barriers;
+
+    private void Start()
+    {
+        _barriers = new List<Transform>();
+    }
+
+    private void Update()
+    {
+        if (_player.position.x > _spawnTrigger)
+        {
+            SpawnBarrier();
+            UpdateSpawnTrigger();
+        }
+    }
+
+    private void UpdateSpawnTrigger()
+    {
+        _spawnTrigger = _player.position.x + _spawnPeriod + Random.Range(-_spawnPeriodRandom, _spawnPeriodRandom);
+    }
+
+    private void SpawnBarrier()
+    {
+        _barriers.Add(CreateBarrier().transform);
+        if (_barriers.Count == _maxBarriers)
+        {
+            Destroy(_barriers[0].gameObject);
+            _barriers.RemoveAt(0);
+        }
+    }
+
+    private Transform CreateBarrier()
+    {
+        var newBarrier =  Instantiate(_barrierTemplate, new Vector3(_player.position.x + _spawnDistance, transform.position.y, 0), Quaternion.identity, transform);
+        return newBarrier.transform;
+    }
+
+}
---
+++ Assets/Scripts/Game/Player.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(PlayerInput))]
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _rightForce;
+    [SerializeField] private float _maxVelocity;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private LayerMask _groundLayer;
+    private Rigidbody2D _body;
+    private CircleCollider2D _collider;
+    private PlayerInput _input;
+    private int _score;
+
+    public UnityAction<int> ScoreChanged;
+
+    private void Awake()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<CircleCollider2D>();
+        _input = GetComponent<PlayerInput>();
+        _score = 0;
+        ScoreChanged?.Invoke(_score);
+
+    }
+
+    private void OnEnable()
+    {
+        _input.Jump += Jump;
+    }
+
+    private void OnDisable()
+    {
+        _input.Jump -= Jump;
+    }
+
+    private void Update()
+    {
+        if (_body.velocity.magnitude <= _maxVelocity)
+            _body.AddForce(new Vector2 (_rightForce,0) * _maxVelocity / (_body.velocity.magnitude + 1) * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Coin coin))
+        {
+            AddScore();
+            Destroy(coin.gameObject);
+        }
+    }
+
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.CircleCast(_collider.bounds.center, _collider.radius, Vector2.down, 0.1f, _groundLayer);
+        return hit.collider != null;
+    }
+
+    private void Jump()
+    {
+        if (IsGrounded())
+        {
+            _body.AddForce(new Vector2(0, _jumpForce));
+        }
+    }
+
+    private void AddScore()
+    {
+        _score += 1;
+        ScoreChanged?.Invoke(_score);
+    }
+}
+
+
